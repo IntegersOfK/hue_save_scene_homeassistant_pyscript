e70c63e32202c39a901276c37955a84886e49b70
Adam James West <ajwest@gmail.com>
2024-03-11 22:40:43 -0400

    Reduce number of calls to lights

--- hue_scene_save.py
+++ hue_scene_save.py
@@ -1,53 +1,32 @@
 import requests
 requests.packages.urllib3.disable_warnings()
 
-
-def update_scene(hue_bridge_ip, hue_api_key, scene_id, lights_actions):
-    log.debug("Creating/updating scene with ID: %s", scene_id)
-    url = f'https://{hue_bridge_ip}/clip/v2/resource/scene/{scene_id}'
-    headers = {'hue-application-key': hue_api_key}
-
-    data = {"type": "scene", "actions": lights_actions}
-    response = task.executor(requests.put, url, headers=headers, json=data, verify=False)
-   
-    return response.json()
-
-@service
-def hue_scene_save(hue_bridge_ip, hue_api_key, hue_scene_id):
-    log.debug("Hue save script started")
-
-    # Fetch scene data
-    url = f'https://{hue_bridge_ip}/clip/v2/resource/scene/{hue_scene_id}'
+def get_all_lights(hue_bridge_ip, hue_api_key):
+    log.debug("Fetching all lights")
+    url = f'https://{hue_bridge_ip}/clip/v2/resource/light'
     headers = {'hue-application-key': hue_api_key}
     response = task.executor(requests.get, url, headers=headers, verify=False)
- 
+    lights_data = response.json()
+    return lights_data
 
-    scene_data = response.json()
+def get_light_states_for_scene(hue_bridge_ip, hue_api_key, light_ids):
+    all_lights_data = get_all_lights(hue_bridge_ip, hue_api_key)
+    filtered_light_states = {}
 
-    # Extract light states directly from the scene data
-    lights_actions = []
-    if 'data' in scene_data and len(scene_data['data']) > 0 and 'actions' in scene_data['data'][0]:
-        for action in scene_data['data'][0]['actions']:
-            if action['target']['rtype'] == 'light':
-                log.debug("Found light with ID: %s in scene", action['target']['rid'])
-                lights_actions.append(action)
+    if 'data' in all_lights_data:
+        for light in all_lights_data['data']:
+            if light['id'] in light_ids:
+                filtered_light_states[light['id']] = light
+                log.debug("Found light with ID: %s in all lights", light['id'])
 
-    # Update scene with the extracted light states
-    scene_response = update_scene(hue_bridge_ip, hue_api_key, hue_scene_id, lights_actions)
+    return filtered_light_states
+
+@service
+def hue_scene_save(hue_bridge_ip, hue_api_key, hue_scene_id):                                                                              
+    log.debug("Hue save script started")                                                                                                   
+                                                                                                                                           
+    scene_lights = get_scene_lights(hue_bridge_ip, hue_api_key, hue_scene_id)                                                              
+    lights_states = get_light_states_for_scene(hue_bridge_ip, hue_api_key, scene_lights)                                                    
+                                                                                                                                           
+    scene_response = update_scene(hue_bridge_ip, hue_api_key, hue_scene_id, lights_states)                                                 
     log.info("Scene creation/update response: %s", scene_response)
-
-
-# This only runs when using from CLI, Home Assistant uses @service with pyscript
-if __name__ == "__main__":
-    import argparse
-    import logging
-    logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
-    log = logging.getLogger(__name__)
-
-    parser = argparse.ArgumentParser(description='To use when not integrated with Home Assistant PyScript')
-    parser.add_argument('-b', '--hue_bridge_ip', required=True)
-    parser.add_argument('-a', '--hue_api_key', required=True)
-    parser.add_argument('-s', '--hue_scene_id', required=True)
-
-    args = parser.parse_args()
-    hue_scene_save(args.hue_bridge_ip, args.hue_api_key, args.hue_scene_id)
